6470ff8a32957673afcec2d9118a3ce94b4e1f7c
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2021-01-09 18:55:29 +0100

    Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -10,13 +11,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetDataComponent } from './requests/get-data/get-data.component';
 import { Chart } from './chart/chart.component';
+import { Header } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GetDataComponent,
-    Chart
+    Chart,
+    Header
+
   ],
   imports: [
     BrowserModule,
@@ -24,7 +28,8 @@ import { Chart } from './chart/chart.component';
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
